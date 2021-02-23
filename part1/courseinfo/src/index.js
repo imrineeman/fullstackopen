11ffa0aa71abe7b0c31634321c5bcdd355fc94ef
Imri Neeman <neemanimri@gmail.com>
2021-02-23 19:28:00 +0200

    ex 1.2 solution

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -13,17 +13,11 @@ const Header = (props) => {
 const Content = (props) => {
   
   return (
-    <React.Fragment>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-    </React.Fragment>
+    <div>
+    <Part part = {props.part1} exercises = {props.exercises1}/>
+    <Part part = {props.part2} exercises = {props.exercises2}/>
+    <Part part = {props.part3} exercises = {props.exercises3}/>
+    </div>
   )
 }
 
@@ -34,6 +28,17 @@ const Total = (props) => {
       Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
     </p>
   )
+}
+
+const Part = (props) => {
+
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+
+  )
+
 }
 
 
