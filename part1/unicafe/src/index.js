b134bee624e9e1b2b063c6c30e91b3b6189abdfa
Imri Neeman <neemanimri@gmail.com>
2021-03-09 11:40:21 +0200

    ex.10 solution

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -10,28 +10,39 @@ const Button = (props) => (
 
 const Statistics = (props) => {
   const { good, neutral, bad } = props
+
   const total = good + neutral + bad
   const average = (good + 0 * neutral + (-1) * bad) / total
   const positive = ((good / (bad + neutral + good)) * 100)
 
-  console.log(good)
+  const getFormula = (formula) => {
+    if (formula === `total`)
+      return total
 
-  if (good === 0 & neutral === 0 & bad === 0) {
-    return (
-      <div>
-        Please Enter Values
-      </div>
-    )
+    else if (formula === `average`)
+      return average
 
-  } else return (
+    else if (formula === `positive`)
+      return positive
+
+    else if (formula === 'good')
+      return good
+
+    else if (formula === `bad`)
+      return bad
+
+    else if (formula === 'neutral')
+      return neutral
+
+    else
+      return <div>
+        No Formula
+    </div>
+  }
+
+  return (
     <div>
-      <h2>Statistics</h2>
-      <p> Good : {good}</p>
-      <p> Neutral : {neutral}</p>
-      <p> Bad : {bad}</p>
-      <p>Total : {total}</p>
-      <p> Average : {average}</p>
-      <p> Positive : {positive}%</p>
+      {props.statistic} : {getFormula(props.formula)}
     </div>
 
   )
@@ -42,16 +53,27 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const stateLength = good + neutral + bad
+  console.log(stateLength)
+
   return (
     <>
       <h1>Give Feedback</h1>
       <Button purpose={`Good`} handleClick={() => setGood(good + 1)} />
       <Button purpose={`Neutral`} handleClick={() => setNeutral(neutral + 1)} />
       <Button purpose={`Bad`} handleClick={() => setBad(bad + 1)} />
-      <Statistics
-        good={good}
-        bad={bad}
-        neutral={neutral} />
+      {
+        stateLength === 0 ? <p> Please enter data </p> :
+          <div>
+            <h2>Statistics</h2>
+            <Statistics statistic={`good`} formula={`good`} good={good} bad={bad} neutral={neutral} />
+            <Statistics statistic={`neutral`} formula={`neutral`} good={good} bad={bad} neutral={neutral} />
+            <Statistics statistic={`bad`} formula={`bad`} good={good} bad={bad} neutral={neutral} />
+            <Statistics statistic={`total`} formula={`total`} good={good} bad={bad} neutral={neutral} />
+            <Statistics statistic={`average`} formula={`average`} good={good} bad={bad} neutral={neutral} />
+            <Statistics statistic={`positive`} formula={`positive`} good={good} bad={bad} neutral={neutral} />
+          </div>
+      }
     </>
   )
 }
