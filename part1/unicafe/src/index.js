dd07e90d3018e569806e7c8b8536d0724b216dd0
Imri Neeman <neemanimri@gmail.com>
2021-03-09 10:46:46 +0200

    ex. 1.8 solution pt.2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -8,6 +8,28 @@ const Button = (props) => (
   </button>
 )
 
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  const average = (good + 0 * neutral + (-1) * bad) / total
+  const positive = ((good / (bad + neutral + good)) * 100)
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p> Good : {good}</p>
+      <p> Neutral : {neutral}</p>
+      <p> Bad : {bad}</p>
+      <p>Total : {total}</p>
+      <p> Average : {average}</p>
+      <p> Positive : {positive}%</p>
+    </div>
+
+  )
+
+}
+
+
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -23,15 +45,10 @@ const App = () => {
       <Button purpose={`Good`} handleClick={() => setGood(good + 1)} />
       <Button purpose={`Neutral`} handleClick={() => setNeutral(neutral + 1)} />
       <Button purpose={`Bad`} handleClick={() => setBad(bad + 1)} />
-      <div>
-        <h2>Statistics</h2>
-        <p> Good : {good}</p>
-        <p> Neutral : {neutral}</p>
-        <p> Bad : {bad}</p>
-        <p>Total : {total}</p>
-        <p> Average : {average}</p>
-        <p> Positive : {positive}%</p>
-      </div>
+      <Statistics
+        good={good}
+        bad={bad}
+        neutral={neutral} />
     </>
   )
 }
