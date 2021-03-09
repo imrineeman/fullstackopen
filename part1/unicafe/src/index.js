e8fb3eb13a3901e02f30c62401d8bae4790fa260
Imri Neeman <neemanimri@gmail.com>
2021-03-09 10:37:43 +0200

    ex. 1.8 solution

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -13,10 +13,13 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const total = bad + neutral + good
+  const average = (good + 0 * neutral + (-1) * bad) / total
+  const positive = ((good / (bad + neutral + good)) * 100)
+
   return (
     <>
       <h1>Give Feedback</h1>
-
       <Button purpose={`Good`} handleClick={() => setGood(good + 1)} />
       <Button purpose={`Neutral`} handleClick={() => setNeutral(neutral + 1)} />
       <Button purpose={`Bad`} handleClick={() => setBad(bad + 1)} />
@@ -25,6 +28,9 @@ const App = () => {
         <p> Good : {good}</p>
         <p> Neutral : {neutral}</p>
         <p> Bad : {bad}</p>
+        <p>Total : {total}</p>
+        <p> Average : {average}</p>
+        <p> Positive : {positive}%</p>
       </div>
     </>
   )
