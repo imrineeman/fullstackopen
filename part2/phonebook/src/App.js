07bbcdc6b2500f23ea8f52ad65959ea0e208d244
Imri Neeman <neemanimri@gmail.com>
2021-03-16 16:57:39 +0200

    2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
+import axios from 'axios'
+
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
 
   const handleFormSubmit = childData => {
     if (persons.some(p => p['name'] === childData.name)) {
@@ -20,6 +17,11 @@ const App = () => {
     }
   }
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(res => setPersons(res.data))
+  }, [])
 
   return (
     <div>
