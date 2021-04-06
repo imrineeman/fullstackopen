d71eed23e43ed21c6805460e88cb21a1c96e84f6
Imri Neeman <neemanimri@gmail.com>
2021-04-06 16:20:05 +0300

    3.22

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,14 +1,14 @@
 //Phonebook web server
 
 require('dotenv').config()
-const { response } = require('express')
-const http = require('http')
+require('express')
+require('http')
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/persons')
-const persons = require('./models/persons')
-const { update } = require('./models/persons')
+require('./models/persons')
+require('./models/persons')
 
 
 const app = express()
@@ -18,83 +18,82 @@ app.use(express.static('build'))
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`);
+  console.log(`Running on port ${PORT}`)
 })
 
 app.use(morgan(function (tokens, req, res) {
-    return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms',
-        console.log(JSON.stringify(req.body))
-    ].join(' ')
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    console.log(JSON.stringify(req.body))
+  ].join(' ')
 }))
 
 const baseUrl = '/api/persons'
 
 app.get('/info', (req, res) => {
-    let length
-    Person.find({}).then(p => {
-        res.send(
-            `<h3>phonebook has info for ${p.length} people</h3>
-        <h4>${date = new Date()}</h4>`)
-    })
+  Person.find({}).then(p => {
+    res.send(
+      `<h3>phonebook has info for ${p.length} people</h3>
+        <h4>${new Date()}</h4>`)
+  })
 })
 
 app.get(baseUrl, (req, res) => {
-    Person.find({}).then(p => res.json(p))
+  Person.find({}).then(p => res.json(p))
 })
 
 app.get(baseUrl + '/:id', (req, res, next) => {
-    Person.findById(req.params.id).then(p => res.json(p)).catch(err => next(err))
+  Person.findById(req.params.id).then(p => res.json(p)).catch(err => next(err))
 })
 
 app.post(baseUrl, (req, res, next) => {
-    const instance = req.body
-    if (instance === undefined) {
-        return res.status(400).json({ error: 'content missing' })
-    } else {
-        const person = new Person({
-            name: instance.name,
-            number: instance.number
-        })
-        person.save()
-            .then(savedPerson => res.json(savedPerson))
-            .catch(err => next(err))
-    }
+  const instance = req.body
+  if (instance === undefined) {
+    return res.status(400).json({ error: 'content missing' })
+  } else {
+    const person = new Person({
+      name: instance.name,
+      number: instance.number
+    })
+    person.save()
+      .then(savedPerson => res.json(savedPerson))
+      .catch(err => next(err))
+  }
 })
 
 app.put(baseUrl + '/:id', (req, res, next) => {
-    const body = req.body
-    const person = {
-        name: body.name,
-        number: body.number
-    }
+  const body = req.body
+  const person = {
+    name: body.name,
+    number: body.number
+  }
 
-    Person.findByIdAndUpdate(req.params.id, person, { new: true })
-        .then(updatedPerson => {
-            res.json(updatedPerson)
-        }).catch(err => next(err))
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    }).catch(err => next(err))
 })
 
 app.delete(baseUrl + '/:id', (req, res, next) => {
-    Person.findByIdAndRemove(req.params.id)
-        .then(result => {
-            res.status(204).end
-        }).catch(err => next(err))
+  Person.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end
+    }).catch(err => next(err))
 })
 
 const errorHandler = (err, req, res, next) => {
-    console.log(err.message);
-    if (err.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' })
-    } if (err.name === 'ValidationError') {
-        return res.status(400).send({ error: 'validation error' })
-    }
+  console.log(err.message)
+  if (err.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } if (err.name === 'ValidationError') {
+    return res.status(400).send({ error: 'validation error' })
+  }
 
-    next(err)
+  next(err)
 }
 app.use(errorHandler)
 
--- part3/phonebook/models/persons.js
+++ part3/phonebook/models/persons.js
@@ -1,46 +1,46 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
-var uniqueValidator = require('mongoose-unique-validator');
+var uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
-console.log('connecting to', url);
+console.log('connecting to', url)
 
 mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
 })
-    .then(result => {
-        console.log('connected to MongoDB');
-    })
-    .catch((err) => {
-        console.log('error connecting to mongoDB', err.message);
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((err) => {
+    console.log('error connecting to mongoDB', err.message)
+  })
 
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true,
-        unique: true,
-    },
-    number: {
-        type: String,
-        minLength: 8,
-        required: true,
-        unique: true,
-    },
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+    unique: true,
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    unique: true,
+  },
 })
 
 personSchema.set('toJSON', {
-    transform: (document, obj) => {
-        obj.id = obj._id.toString()
-        delete obj._id
-        delete obj.__v
-    }
+  transform: (document, obj) => {
+    obj.id = obj._id.toString()
+    delete obj._id
+    delete obj.__v
+  }
 })
 
 personSchema.plugin(uniqueValidator)
