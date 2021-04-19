5d7a445bd2984460291854122a66b5ac764109b6
Imri Neeman <neemanimri@gmail.com>
2021-04-19 11:03:49 +0300

    5.3

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
@@ -5,10 +6,16 @@ import loginService from './services/login'
 import userService from './services/users'
 
 const App = () => {
+
   const [blogs, setBlogs] = useState([])
+
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+
+  const [blogTitle, setBlogTitle] = useState('')
+  const [blogAuthor, setBlogAuthor] = useState('')
+  const [blogUrl, setBlogUrl] = useState('')
 
   const handleLogin = async () => {
     const userNew = {
@@ -31,15 +38,22 @@ const App = () => {
     window.localStorage.removeItem('loggedUser')
   }
 
+  const handleBlogSubmit = async () => {
+    const newBlog = {
+      "title": blogTitle,
+      "author": blogAuthor,
+      "url": blogUrl
+    }
+    blogService.setToken(user.token)
+    const res = await blogService.create(newBlog)
+    console.log(res);
+  }
+
   useEffect(() => {
     if (user !== null) {
       getUserBlogs()
     }
   }, [user])
-
-  useEffect(() => {
-    console.log(blogs);
-  }, [blogs])
 
   useEffect(() => {
     const loggedOnJSON = window.localStorage.getItem('loggedUser')
@@ -69,6 +83,17 @@ const App = () => {
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
+          <h2>Create new</h2>
+          <p>Title:</p><input
+            onChange={({ target }) => setBlogTitle(target.value)}
+          ></input>
+          <p>Author:</p><input
+            onChange={({ target }) => setBlogAuthor(target.value)}
+          ></input>
+          <p>URL:</p> <input
+            onChange={({ target }) => setBlogUrl(target.value)}
+          ></input>
+          <button onClick={handleBlogSubmit}>Submit</button>
         </div>}
     </div>
   )
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,25 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3005/api/blogs'
 
+
+let token = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+
 const getAll = async () => {
   const request = await axios.get(baseUrl)
   return request.data
 }
 
-export default { getAll }
+const create = async newBlog => {
+  const conf = {
+    headers: { Authorization: token },
+  }
+
+  const res = await axios.post(baseUrl, newBlog, conf)
+  return res.data
+}
+
+export default { getAll, create, setToken }
